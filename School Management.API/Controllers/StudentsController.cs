240b4841d6e13dbaaf2f1b11fde8a491c2cad01d
venkat.5160 <venkat.5160@gmail.com>
2025-08-20 12:48:44 +0530

    Changes

--- School Management.API/Controllers/StudentsController.cs
+++ School Management.API/Controllers/StudentsController.cs
@@ -1,6 +1,7 @@
 ﻿
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Core.Types;
 using SchoolManagement.DAL.DataContext;
 using SchoolManagement.DAL.Entities;
 
@@ -39,9 +40,9 @@ namespace School_Management.API.Controllers
         }
 
         // PUT: api/Students/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // api/Students/11?iid=11&name=22'
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutStudent(int id, Student student)
+        public async Task<IActionResult> PutStudent(int id,[FromQuery] string name, Student student)
         {
             if (id != student.Id)
             {
@@ -69,6 +70,7 @@ namespace School_Management.API.Controllers
             return NoContent();
         }
 
+        // Parameter: Student student (model binding from body, but no explicit [FromBody] attribute)
         // POST: api/Students
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
@@ -111,6 +113,39 @@ namespace School_Management.API.Controllers
 
             return NoContent();
         }
+
+        //	Parameter: [FromBody] Student student (explicitly binds from request body)
+        //Adds a new Student(POST api/student).
+        [HttpPost("create")]
+        public async Task<ActionResult<Student>> CreateStudent([FromBody] Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _context.Students.Add(student);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetStudent", new { id = student.Id }, student);
+        }
+
+        [HttpGet("Search")]
+        public async Task<ActionResult<IEnumerable<Student>>> SearchStudent([FromQuery] string? name, [FromQuery] string? address, [FromQuery] string? phoneNumber)
+        {
+            var filteredStudents = _context.Students.AsQueryable();
+            if (!string.IsNullOrEmpty(name))
+                filteredStudents = filteredStudents.Where(e => e.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+            if (!string.IsNullOrEmpty(address))
+                filteredStudents = filteredStudents.Where(e => e.Address.Equals(address, StringComparison.OrdinalIgnoreCase));
+            if (!string.IsNullOrEmpty(phoneNumber))
+                filteredStudents = filteredStudents.Where(e => e.PhoneNumber.Equals(phoneNumber, StringComparison.OrdinalIgnoreCase));
+            var result = await filteredStudents.ToListAsync();
+            if (!result.Any())
+                return NotFound("No student match the provided search criteria.");
+            return Ok(result);
+        }
+
+
+
 
         private bool StudentExists(int id)
         {
